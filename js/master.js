2396bd18ce964592b209bef43b4ea2880c2f5285
ayoubchaaoui <75631202+ayoubchaaoui@users.noreply.github.com>
2020-12-09 20:09:25 +0100

    Add files via upload

---
+++ js/master.js
@@ -0,0 +1,10 @@
+function imgSlid(src) {
+
+    document.querySelector('.img').src = src ;
+
+}
+function bgChenger (color){
+
+    document.querySelector('.landing').style.background = color ;
+
+}
